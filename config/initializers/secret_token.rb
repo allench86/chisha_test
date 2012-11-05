005240aac4f5f4921de0d84fb7d4363a34b5ec22
Kai Chen <allench86@gmail.com>
2012-11-04 21:38:56 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChishaTest::Application.config.secret_token = '6a07e9534117b088a1337bbcaf1b0cad53c44f2c57808934dced3a4ff44614c4c9bde183a7f937b174c96eb49256839b2fab9f9e35b7f663942339c613a4e5be'
